5aa08313300fe2afc828b12fc68bf653051db37a
ayah <1the.a.empire@gmail.com>
2026-08-23 02:54:27 +0200

    Dto  request and respond

--- src/main/java/com/mohasabah/dtos/request/RegisterRequestDto.java
+++ src/main/java/com/mohasabah/dtos/request/RegisterRequestDto.java
@@ -6,18 +6,27 @@ import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Email
 @Builder
 @Data
 
 public class RegisterRequestDto {
 
+
+    @NotBlank private String firstName ;
+    @NotBlank private String lastName;
+    @NotBlank private LocalDate birthday;
     @NotBlank
     @Email
     String email;
-    @NotBlank private String firstName ;
-    @NotBlank private String lastName;
+    @NotBlank  @Email String emailConfirm;
+    // todo change html password to 8 charchters instead of 6
     @NotBlank private  @Size  (min = 8 ) String password;
+    @NotBlank private  @Size  (min = 8 ) String passwordConfirm;
+
+
 
 
 }
